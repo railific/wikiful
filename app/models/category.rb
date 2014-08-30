299e0a45deb4e727d5cb301eb3523ff18615c147
Marty <railific@google.com>
2014-08-30 11:42:16 +1000

    fixed missing comma in Categories model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
   has_many :article_categories
-  has_many :articles through: :article_categories
+  has_many :articles, through: :article_categories
   
   validates :name, uniqueness: true
   
