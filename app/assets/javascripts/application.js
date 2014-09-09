a95c7035e0fe3c6c99016373fbd222aa7bfa0e48
Marty <railific@google.com>
2014-09-10 06:36:22 +1000

    bootstrap fix attempt

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
 //= require jquery
 //= require jquery_ujs
-//= require twitter/bootstrap
 //= require turbolinks
+//= require twitter/bootstrap
 //= require_tree .
